63308ce9c4bdbbc9b85ef6f8d3a13271e4c99ed5
Yashwanth R <125284177+YashwanthR05@users.noreply.github.com>
2026-03-14 11:52:47 +0530

    updated

--- src/main/java/com/employee/management/controlller/EmployeeController.java
+++ src/main/java/com/employee/management/controlller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.employee.management.controlller;
 import java.awt.PageAttributes.MediaType;
 import java.io.IOException;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,12 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.employee.management.dto.EmployeeRequest;
 import com.employee.management.exception.BadRequestException;
 import com.employee.management.model.Employee;
 import com.employee.management.service.EmployeeService;
-
 import jakarta.validation.Valid;
 
 @RestController
